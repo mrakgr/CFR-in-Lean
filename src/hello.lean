d6d7b1a214bea29fb6b5eefb6e75ac578abe2bdb
Marko Grdinić <mrakgr@gmail.com>
2019-10-29 19:30:52 +0100

    Done for the day here.

--- src/hello.lean
+++ src/hello.lean
@@ -1,9 +1,44 @@
 import data.rat
-import data.list data.vector
 
-inductive InfosetedGame {infoset_count : nat} (infoset_sizes : fin infoset_count → ℕ)
-| Terminal (reward : rat) : InfosetedGame
-| Response (infoset_id : fin infoset_count) (subnodes : fin (infoset_sizes infoset_id) → InfosetedGame) : InfosetedGame
+def Infoset := ℕ 
+def Size := ℕ
+@[derive [has_neg, has_one]] def Player := ℤ
 
+inductive GameTree (infoset_sizes : Infoset → Size)
+| Terminal (reward : ℚ) : GameTree
+| Response (id : Infoset) (subnodes : fin (infoset_sizes id) → GameTree) : GameTree
 
+structure Policy (infoset_sizes : Infoset → Size) := 
+    (val : ∀ (id : Infoset), fin (infoset_sizes id) → ℚ)
+    (wf : ∀ (id : Infoset) (i : fin (infoset_sizes id)), 0 <= val id i ∧ val id i <= 1)
 
+variable {infoset_sizes : Infoset → Size}
+
+def nat.foldl.fin_template {α : nat → Type*} (n' : nat) 
+    : ∀ (n : fin n') (s : α 0) (f : ∀ (n : fin n'), (α n.val) → α (n.val+1)), α n.val
+| ⟨0, _⟩ s f := s
+| ⟨n+1, lt⟩ s f := let n' : fin n' := ⟨n, buffer.lt_aux_1 lt ⟩ in f n' (nat.foldl.fin_template n' s f)
+
+def nat.foldl.fin {α : nat → Type*} 
+    : ∀ (n : nat) (s : α 0) (f : ∀ (n : fin n), (α n.val) → α (n.val+1)), α n
+| 0 s f := s
+| (n+1) s f := 
+    let n' : fin (n+1) := ⟨ n, lt_add_one n ⟩ in
+    f n' (nat.foldl.fin_template (n+1) n' s f)
+
+def u.template_calc (σ : Policy infoset_sizes) : GameTree infoset_sizes → ℚ
+| (GameTree.Terminal _ reward) := reward
+| (GameTree.Response id' subnodes) :=
+    let σ := σ.val id' in
+    @nat.foldl.fin (fun i, rat) (infoset_sizes id') 0 (fun i s, s + σ i * u.template_calc (subnodes i))
+
+def u.template (p : Player) (σ : Policy infoset_sizes) (id : Infoset) : Player → GameTree infoset_sizes → ℚ
+| p' (GameTree.Terminal _ reward) := 0
+| p' (GameTree.Response id' subnodes) := 
+    if p = p' ∧ id = id' then
+        let σ' := σ.val id' in
+        @nat.foldl.fin (fun i, rat) (infoset_sizes id') 0 $ fun i s, s + σ' i * u.template_calc σ (subnodes i)
+    else 
+        @nat.foldl.fin (fun i, rat) (infoset_sizes id') 0 $ fun i s, s + u.template (-p') (subnodes i)
+            
+def u (tree : GameTree infoset_sizes) (p : Player) (σ : Policy infoset_sizes) (id : Infoset) : ℚ := u.template p σ id 1 tree
