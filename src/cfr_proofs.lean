8c8b71a0efd6c88ae568a8796fa10925fdf4a280
Marko Grdinić <mrakgr@gmail.com>
2019-10-29 11:38:27 +0100

    Finally defined lemma 5.

---
+++ src/cfr_proofs.lean
@@ -0,0 +1,43 @@
+import data.rat
+
+constant Infoset : Type
+constant Policy : Type
+
+def maximize {α : Type} : α × list α → (α → ℚ) → ℚ | (x,xs) f := xs.foldl (fun s a, max s (f a)) (f x)
+def list.fsum {α : Type} : (α → ℚ) → list α → ℚ | f l := l.foldl (fun s a, s + f a) 0
+-- Compared to the paper, here I am moving the indexing so it obeys the C notation.
+def nat.fsum (f : ℕ → ℚ) : ℕ → ℚ | 0 := 0 | (n+1) := n.fsum + f n
+
+constant σ : ℕ → Policy
+constant σ_all : (ℕ → Policy) × list (ℕ → Policy)
+constant Policy.update_at_infosets : Policy → list Infoset → Policy → Policy
+
+constant Player : Type
+constant utility : Player → Policy → Infoset → ℚ
+constant D : Infoset → list Infoset
+constant Succ : Player → Infoset → list Infoset
+
+constant path : Player → Policy → rat
+constant path_is_probability : ∀ i σ, 0 <= path i σ ∧ path i σ <= 1
+constant opposite_player : Player → Player
+
+def R.template (update_policy : Policy → Infoset → Policy → Policy) 
+    (i : Player) (T : ℕ) (I : Infoset) : ℚ := 
+    1 / T * 
+    maximize σ_all (fun σ',
+        T.fsum (fun t,
+            let (σ, σ') := (σ t, σ' t) in
+            let σ' := update_policy σ' I σ in
+            path (opposite_player i) σ * (utility i σ' I - utility i σ I)
+            )
+        )
+        
+def R.full := R.template (fun σ' I σ, σ.update_at_infosets (D I) σ')
+def R.full.pos (i : Player) (T : ℕ) (I : Infoset) := max (R.full i T I) 0
+def R.immediate := R.template (fun σ' I σ, σ.update_at_infosets [I] σ')
+def R.immediate.pos (i : Player) (T : ℕ) (I : Infoset) := max (R.immediate i T I) 0
+
+def lemma_5 (i : Player) (T : ℕ) (I : Infoset) 
+        : R.full i T I <= R.immediate i T I + (Succ i I).fsum (fun I', R.full.pos i T I) := 
+    sorry
+    
--- src/hello.lean
+++ src/hello.lean
@@ -1,18 +1,9 @@
 import data.rat
 import data.list data.vector
 
-inductive Game (players : list nat) 
-| Terminal (player : {i // i ∈ players}) (reward : rat) : Game
-| Response (player : {i // i ∈ players}) (subnodes : list Game) : Game 
-| Chance (player : {i // i ∈ players}) (subnodes : list Game) : Game
+inductive InfosetedGame {infoset_count : nat} (infoset_sizes : fin infoset_count → ℕ)
+| Terminal (reward : rat) : InfosetedGame
+| Response (infoset_id : fin infoset_count) (subnodes : fin (infoset_sizes infoset_id) → InfosetedGame) : InfosetedGame
 
-inductive InfosetedGame (players : list string) (infoset_count : nat) (infoset_sizes : fin infoset_count → ℕ)
-| Terminal (player : {i // i ∈ players}) (reward : rat) : InfosetedGame
-| Response
-    (player : {i // i ∈ players})
-    (infoset_id : fin infoset_count)
-    (subnodes : fin (infoset_sizes infoset_id) → InfosetedGame)
-    : InfosetedGame
-| Chance (player : {i // i ∈ players})
-    {n} (subnodes : fin n → InfosetedGame) : InfosetedGame
+
 
